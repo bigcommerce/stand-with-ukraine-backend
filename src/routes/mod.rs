2fdfe4404ee2562b03281e8af1208d6c00750217
BC Micah <micah.thomas@bigcommerce.com>
2022-04-25 19:01:44 -0600

    Initial setup for DigitalOcean Hosting

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -11,12 +11,13 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     let bearer_auth_config = Config::default().realm("api-v1").scope("modify");
     let auth_validator = HttpAuthentication::bearer(validate_jwt_bearer_token);
 
+    cfg.service(web::resource("/health_check").route(web::get().to(api::health_check)));
+
     cfg.service(
         web::scope("/api/v1")
             .app_data(bearer_auth_config)
             .wrap(auth_validator)
             .wrap(Cors::permissive())
-            .route("/health_check", web::get().to(api::health_check))
             .route("/save", web::post().to(api::save_widget_configuration))
             .route("/publish", web::post().to(api::publish_widget))
             .route("/remove", web::delete().to(api::remove_widget)),
