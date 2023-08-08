a2c46b95ac90ba2c6d8ae4e43df94a8bd08c12c2
Micah Thomas <micah.thomas@bigcommerce.com>
2023-08-08 16:26:10 -0600

    chore: fix the sqlx cache

--- scripts/generate_offline_query_data.sh
+++ scripts/generate_offline_query_data.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 export SQLX_OFFLINE=true
-cargo sqlx prepare --workspace
+PROJECT_DIR=$(pwd)
+
+cd $PROJECT_DIR/apps/server
+cargo sqlx prepare \
+    -- --lib
+
+cd $PROJECT_DIR/apps/exporter
+cargo sqlx prepare \
+    -- --bin swu-exporter
--- scripts/pre_push.sh
+++ scripts/pre_push.sh
@@ -6,4 +6,14 @@ cargo clippy -- -D clippy::all
 #cargo clippy --all-features -- --deny warnings --deny clippy::pedantic --deny clippy::nursery
 cargo clippy --all-features -- --deny warnings --deny clippy::nursery
 
-cargo sqlx prepare --check --workspace
+PROJECT_DIR=$(pwd)
+
+cd $PROJECT_DIR/apps/server
+cargo sqlx prepare \
+    --check \
+    -- --lib
+
+cd $PROJECT_DIR/apps/exporter
+cargo sqlx prepare \
+    --check \
+    -- --bin swu-exporter
